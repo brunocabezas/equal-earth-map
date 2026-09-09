7ba0d8ff2d7f73483139f58c3a531114a78caec3
Bruno Cabezas <bruno.cabezas@gmail.com>
2026-09-09 02:00:12 +0200

    Remove unused Google Search Console HTML file.
    Ownership is already verified with the meta tag in index.html.

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 window.EQUAL_EARTH_VERSION = {
-  commit: "49e59a0715fd5a7c53ba38f2af3f93393b525af2",
-  short: "49e59a0",
-  builtAt: "2026-09-09T01:54:17+02:00"
+  commit: "f7c1ec5200634ab20cdf5dbd5b41370f9ffb363e",
+  short: "f7c1ec5",
+  builtAt: "2026-09-09T02:00:12+02:00"
 };
